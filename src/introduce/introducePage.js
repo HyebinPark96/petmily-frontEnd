27a974b0c2aabf63e32821a1e2edcb6935c4b2f3
HyebinPark96 <bini2718@naver.com>
2023-06-28 20:08:15 +0900

    API Key 비공개 처리

--- src/introduce/introducePage.js
+++ src/introduce/introducePage.js
@@ -1,6 +1,20 @@
+import Axios from 'axios';
 import React from 'react'
+import { useEffect } from 'react';
 
 const Introduce = () => {
+  useEffect(() => {
+    // console.log(process.env.REACT_APP_API_KEY);
+    
+    Axios.get(`http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?bgnde=20230101&endde=20230627&pageNo=1&numOfRows=10&serviceKey=` + process.env.REACT_APP_API_KEY)
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }, [])
+
   return (
     <div>introducePage</div>
   )
