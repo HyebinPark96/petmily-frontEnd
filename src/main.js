3e4844e8ce5593e2686841cbb53b287ad99045af
hjchoi0207 <chjin95@gmail.com>
2023-06-30 21:39:29 +0900

    tailwindcss 적용

--- src/main.js
+++ src/main.js
@@ -3,9 +3,17 @@ import React from 'react'
 const Main = () => {
   return (
     <>
-        <div>main</div>
+    <div class="grid grid-cols-4 gap-4">
+      <div>01</div>
+      <div>02</div>
+      <div>03</div>
+      <div>04</div>
+      <div>05</div>
+    </div>
     </>
-  )
-}
+  );
+};
+
+
 
 export default Main
