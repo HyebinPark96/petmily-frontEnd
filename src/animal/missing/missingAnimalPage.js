f9ac8b167575adf8b4369d61f2ca8aeccab3c9a1
HyebinPark96 <bini2718@naver.com>
2023-07-11 19:32:58 +0900

    동물 리스트 무한 스크롤 적용

--- src/animal/missing/missingAnimalPage.js
+++ src/animal/missing/missingAnimalPage.js
@@ -10,54 +10,98 @@ import { Button, CardActionArea, CardActions } from '@mui/material';
 const MissingAnimalPage = () => {
 
   const [missingAnimalList, setMissingAnimalList] = useState([]); 
+  const [pageNo, setPageNo] = useState(1);
+  const [numOfRows, setNumOfRows] = useState(12);
+  const [fetching, setFetching] = useState(false); // 추가 데이터를 로드하는지 아닌지를 담기위한 state
 
-  const getMissingAnimalList = () => {
-    axios.get('/api/animal/selectMissingAnimalList')
+  const getMissingAnimalList = async() => {
+    // 추가 데이터를 로드하는 상태로 전환
+    setFetching(true);
+
+    await axios.get('/api/animal/selectMissingAnimalList', {
+      params: {
+        pageNo: pageNo,
+        numOfRows: numOfRows,
+      }
+    })
     .then((result) => {
-      setMissingAnimalList(result.data.response.body.items.item);
+      setMissingAnimalList([ ...missingAnimalList, ...result.data ]);
+      setPageNo(pageNo + 1);
     })
     .catch((err) => {
       console.log(err);
-    })
+    });
+
+    // 추가 데이터 로드 끝
+    setFetching(false);
   }
+
+  // 스크롤 이벤트 핸들러
+  const handleScroll = () => {
+    // 페이지 총 높이
+    const scrollHeight = document.documentElement.scrollHeight;
+    
+    // 이미 스크롤되어 보이지 않는 구간 높이
+    const scrollTop = document.documentElement.scrollTop;
+    
+    // 현재 사용자에게 보여지는 페이지 높이
+    const clientHeight = document.documentElement.clientHeight;
+
+    // 페이지 끝에 도달했다면 다음 데이터 로드
+    if (scrollTop + clientHeight >= scrollHeight && fetching === false) {
+      getMissingAnimalList();
+    }
+  };
+
+  useEffect(() => {
+    // 스크롤 이벤트 등록
+    window.addEventListener("scroll", handleScroll);
+
+    // unmount 시 스크롤 이벤트 해제 
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
 
   useEffect(() => {
     getMissingAnimalList();
   }, [])
 
   return (
-      <div class="cards-container">
-        {missingAnimalList.map((missingAnimal) => ( 
-          <Card sx={{ maxWidth: 300 }} className="card">
-            <CardActionArea>
+    <div class="cards-container">
+      {missingAnimalList.map((missingAnimal, idx) => ( 
+        <Card key="idx" className="card">
+          <CardActionArea className="card-action-area">
+            <div className="card-media-container">
               <CardMedia
                 className="card-media"
                 component="img"
-                src={missingAnimal.filename}
-                alt="green iguana"
+                src={missingAnimal?.filename}
+                alt="image"
               />
-              <CardContent className="card-content">
-                <Typography gutterBottom variant="h5" component="div">
-                  {missingAnimal?.kindCd} 
-                </Typography>
-                <Typography variant="div" component="div">
-                  공고기간: {missingAnimal?.noticeSdt} ~ {missingAnimalList?.noticeEdt}
-                </Typography>
-                <div className="care-addr">
+            </div>
+            <CardContent className="card-content">
+              <Typography gutterBottom variant="h5" component="div">
+                {missingAnimal?.kindCd} 
+              </Typography>
+              <Typography variant="div" component="div">
+                공고기간: {missingAnimal?.noticeSdt} ~ {missingAnimalList?.noticeEdt}
+              </Typography>
+              <div className="care-addr">
                 <Typography variant="body2" color="text.secondary">
                   보호장소: {missingAnimalList?.careAddr}
                 </Typography>
-                </div>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-                Share
-              </Button>
-            </CardActions>
-          </Card>
-        ))}
-      </div>
+              </div>
+            </CardContent>
+          </CardActionArea>
+          {/* <CardActions>
+            <Button size="small" color="primary">
+              Share
+            </Button>
+          </CardActions> */}
+        </Card>
+      ))}
+    </div>
   )
 }
 
