71943ad25899cc40b1d3c19007689a9f12732572
HyebinPark96 <bini2718@naver.com>
2023-09-10 14:09:58 +0900

    netlify 임시 설정 중

--- src/animal/list/animalListPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AnimalListPage = () => {
-  return (
-    <div>animalListPagA</div>
-  )
-}
-
-export default AnimalListPage
---
+++ src/js/contants.js
@@ -0,0 +1,2 @@
+export const API_SEARCH_ENDPOINT =
+  'https://suspicious-tesla-29e2bd.netlify.app/.netlify/functions/myFunction/search';
