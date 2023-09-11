7ff466e310f15a75fa6b217a996ef22f97f93f42
HyebinPark96 <bini2718@naver.com>
2023-09-11 22:13:54 +0900

    functions 수정

--- netlify/functions/getMissingAnimal.js
+++ netlify/functions/getMissingAnimal.js
@@ -1,4 +1,4 @@
-//netlify/functions/getPhotos.js 
+//netlify/functions/getPhotos.js
 require("dotenv").config();
 
 const axios = require("axios");
@@ -6,12 +6,21 @@ const axios = require("axios");
 exports.handler = async (event, context) => {
   try {
     // const { keyword } = event.queryStringParameters;
-    let result = await axios.get(
-        `https://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?_type=json&pageNo=1&numOfRows=10&serviceKey=${process.env.REACT_APP_API_KEY}`
+    let response = await axios.get(
+      `https://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?_type=json&pageNo=1&numOfRows=10&serviceKey=${process.env.REACT_APP_API_KEY}`,
+      {
+        params: {
+          serviceKey: process.env.REACT_APP_API_KEY,
+          numOfRows: 1,
+          pageNo: 10,
+        },
+      }
     );
 
-    let item = result.data;
-    return item;
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data),
+    };
 
   } catch (error) {
     return {
